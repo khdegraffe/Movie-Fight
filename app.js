655ddd2cb21409f0856eb4df82b1412c110eba19
khdegraffe <khdegraffe@gmail.com>
2025-07-20 21:12:43 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//  http://www.omdbapi.com/?apikey=[23dcd452]&
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "23dcd452",
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+
+
+const onInput = debounce((event) => {
+  fetchData(event.target.value);
+});
+
+input.addEventListener("input", debounce(onInput, 500));
